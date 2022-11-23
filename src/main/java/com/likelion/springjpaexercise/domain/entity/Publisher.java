9fe93e52d302583e6d478fb085ccef07d2370831
서정희 <sjhlko@naver.com>
2022-11-23 15:31:27 +0900

    출판사 보이도록 수정

---
+++ src/main/java/com/likelion/springjpaexercise/domain/entity/Publisher.java
@@ -0,0 +1,19 @@
+package com.likelion.springjpaexercise.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Publisher {
+    @Id
+    private Integer id;
+    public String name;
+    public String address;
+}
